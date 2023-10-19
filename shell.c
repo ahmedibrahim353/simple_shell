13f1d1e77f1a4d3afa74424bba5e39ef66b2da7a
ahmedibrahim <ahmadibrahim1234567890@gmail.com>
2023-10-18 18:25:06 -0700

    fixall

---
+++ shell.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * main - entry point
+ * @ac: arg count.
+ * @av: arg vector.
+ * Return:  0 or 1 or NULL.
+ */
+int main(int ac, char **av)
+{
+	info_t info[] = { INIT_INFO };
+
+	(void)ac;
+
+	pop_env_list(info);
+	re_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
+}
